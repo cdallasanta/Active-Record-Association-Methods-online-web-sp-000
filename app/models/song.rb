37353ab43bf3744e28112fa22fe75a174e7f315f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-18 18:20:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     binding.pry
-    self.artist = Artist.find_by(name:"Drake")
+    self.artist = Artist.find_or_create_by(name:"Drake")
     self.save
   end
 end
