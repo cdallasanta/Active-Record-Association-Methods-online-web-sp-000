c023e8c1307988290f525f39a8e4379345d302cb
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-18 17:56:11 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,8 +3,10 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
+    self.artist = Artist.find_by(name:"Drake")
   end
 end
